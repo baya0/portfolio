c554cf7c019ecdd0e24d3f101ccf6c7db422f58c
bayan_ar <91153108+baya0@users.noreply.github.com>
2025-09-08 00:18:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js - Updated for GitHub Pages
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production'
-const repo = 'bayan-portfolio' // Replace with your actual repository name
+const repo = 'portfolio'
 
 const nextConfig = {
   reactStrictMode: true,
